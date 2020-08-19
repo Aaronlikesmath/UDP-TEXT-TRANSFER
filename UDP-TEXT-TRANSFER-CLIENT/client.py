38241f88f179993474f5107020b7b54714d6dc81
AaronLikesMath <github@aaronlowitzsch.com>
2020-08-19 12:07:59 +0200

    Im out

--- UDP-TEXT-TRANSFER-CLIENT/client.py
+++ UDP-TEXT-TRANSFER-CLIENT/client.py
@@ -5,7 +5,7 @@ from datetime import datetime
 import rsa
 
 # Setting up parsing lib
-UTEXT = UDPTEXTPARSE("clientconf.js")
+UTEXT = UDPTEXTPARSE()
 
 def setup(conf_file):
     JS = JsonWrap(conf_file)
@@ -37,15 +37,16 @@ while True:
         print (UTEXT.messagehandler(data, address))
     
     elif select.lower() == "e":
-        data, address = US.s.recvfrom(4096)
-        select = input("What do you want to do? [G(Generate new keys)/I(Import keys)/E(Export keys)/D(Decrypt message using current key)/E(Send Encrypted msg)]")
+        select = input("What do you want to do? [G(Generate new keys)/I(Import keys)/EX(Export keys)/D(Decrypt message using current key)/E(Send Encrypted msg)]")
         if select.lower() == "g":
             public_key, private_key = encrypgen()
         elif select.lower() == "i":
+            data, address = US.s.recvfrom(4096)
             e_public_key = data.decode("utf-8")
-        elif select.lower() == "e":
+        elif select.lower() == "ex":
             US.sendpacket(public_key, IP, port)
         elif select.lower() == "d":
+            data, address = US.s.recvfrom(4096)
             msg = rsa.decrypt(data.decode("utf-8"), private_key)
             print (f"\nMessage: {msg}\nFrom: {list(address)[0]}\nTime: {datetime.now()}")
         elif select.lower() == "e":
